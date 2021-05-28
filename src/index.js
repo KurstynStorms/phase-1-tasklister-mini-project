edfb6f08aa6c1755a770ba851e60907ecc1c464b
Kurstyn Storms <kurstyn.storms@powerhrg.com>
2021-05-28 10:15:11 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const unorderedList = document.querySelector('#tasks')
+  const form = document.querySelector('#create-task-form')
+  const input = document.querySelector("#new-task-description")
+  form.addEventListener('submit', function(event) {
+    event.preventDefault()
+  const newListItem = document.createElement('li') 
+  unorderedList.appendChild(newListItem)
+  newListItem.textContent = input.value
+  })
+})
+
+
